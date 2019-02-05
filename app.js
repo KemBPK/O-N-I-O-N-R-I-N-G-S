1f98c2eebcb72769638af66be1ac6d595bd47466
KemBPK <kembpk@gmail.com>
2019-02-05 10:58:12 -0800

    added more example codes

--- app.js
+++ app.js
@@ -6,6 +6,7 @@
 var express = require('express');
 var app = express();
 
+app.use(express.urlencoded()); //allows retrievals of post parameters
 
 app.set('view engine', 'ejs');
 
@@ -14,24 +15,33 @@ app.get('/', function (req, res) {
 })
 
 app.get('/get_input', function (req, res) {
-    var input = req.query.input
+    var input = req.query.input //get parameter
     var unirest = require('unirest'); //npm install unirest
     unirest.get("https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/food/menuItems/search?query=" + input)
         .header("X-RapidAPI-Key", "bc37d905e4msh4f3080a6fc02ccdp136f37jsnbe8cef869ce7")
         .end(function (result) {
 
-            console.log(Object.keys(result.body.menuItems[0]));
+            //console.log(Object.keys(result.body.menuItems[0])); //View object properties inside the command prompt
             res.render('index', {
                 input: input,
                 result: result.body.menuItems
             });
-            //console.log(/*result.status, result.headers,*/ result.body);
+            //console.log(/*result.status, result.headers,*/ result.body); //View api query result inside the command prompt
         });
     //res.render('index');
     //res.render('index', {
     //    input : input
     //});
 })
+
+app.post('/test_post', function (req, res) {
+    var input = req.body.input //post parameter
+    console.log(input);
+    res.redirect('/get_input');
+})
+
+
+
 
 
 
