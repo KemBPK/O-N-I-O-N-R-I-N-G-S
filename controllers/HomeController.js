22c9e55efd9bb448564ccf080441c07f5075462d
KemBPK <kembpk@gmail.com>
2019-03-01 12:50:33 -0800

    example code for google map

--- controllers/HomeController.js
+++ controllers/HomeController.js
@@ -18,13 +18,20 @@ module.exports = function(app, yelp) {
           };
         search = yelp.SearchPlaces(input);
 
-        res.render('./Home/Search', {
-            input: input,
-            result: search
+        // res.render('./Home/Search', {
+        //     input: input,
+        //     result: search
+        // });
+
+        search.then(function(response){
+        // console.log(response.jsonBody);
+        return response.jsonBody.businesses;
+        }).then(function(result){
+            res.render('./Home/Search', {
+                input: input,
+                result: result
+            });
         });
-        //   search.then(function(response){
-        //     console.log(response.jsonBody);
-        //   });
     })
     
     app.post('/Home/get_input', function (req, res) {
