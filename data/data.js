b91f0d21783e5edd64628a0c2da77a82a8688267
KemBPK <kembpk@gmail.com>
2019-04-07 00:01:18 -0700

    done with mysql connection

--- data/data.js
+++ data/data.js
@@ -5,14 +5,17 @@ var mysql = require('mysql');
 function connection(){
     var con = mysql.createConnection({
 
-        host     : process.env.RDS_HOSTNAME,
-        user     : process.env.RDS_USERNAME,
-        password : process.env.RDS_PASSWORD,
-        port     : process.env.RDS_PORT
-        // host: "aa10bua224idnik.cvo7xscrgkdh.us-west-1.rds.amazonaws.com",
-        // port: 3306,
-        // user: "orwwDB",
-        // password: "z5zuQ5G6r4CiVmF"
+        //deployment code
+        // host     : process.env.RDS_HOSTNAME,
+        // user     : process.env.RDS_USERNAME,
+        // password : process.env.RDS_PASSWORD,
+        // port     : process.env.RDS_PORT
+
+        //testing code
+        host: "aa10bua224idnik.cvo7xscrgkdh.us-west-1.rds.amazonaws.com",
+        port: 3306,
+        user: "orwwDB",
+        password: "z5zuQ5G6r4CiVmF"
       });
     console.log('Attempting to connect...\n');
 
