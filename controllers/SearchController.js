adbc3c1593d719235200da9c541f71b3d5178ca4
KemBPK <kembpk@gmail.com>
2019-04-19 16:00:02 -0700

    Working on modal

--- controllers/SearchController.js
+++ controllers/SearchController.js
@@ -1,5 +1,18 @@
 'use strict';
 
 module.exports = function(app, yelp, db) {
-    
+    app.get('/Search/Map', function (req, res) {
+        var name = req.query.restaurant;
+        name = name.replace(/[-]/g, ' ');
+        name = name.replace(/[0-9]/g, '');
+        const input = {
+            restaurant: name.toUpperCase(),
+            latitude: req.query.latitude,
+            longitude: req.query.longitude 
+          };
+        res.render('./Partials/_map',{
+            input: input
+        });    
+        //res.render('./Home/Welcome'); //relative to the root view folder
+    })
 }
