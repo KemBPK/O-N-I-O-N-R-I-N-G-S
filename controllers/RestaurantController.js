cf721b0477bf618f492b45676ab72c3da604ce42
ArthurDayot <adayot0@saddleback.edu>
2019-04-28 18:49:53 -0700

    states and output reviews on restaurant profile

--- controllers/RestaurantController.js
+++ controllers/RestaurantController.js
@@ -1,6 +1,6 @@
 'use strict';
 
-module.exports = function(app, yelp, db) {
+module.exports = function (app, yelp, db) {
     app.get('/Restaurant/Map', function (req, res) {
         var name = req.query.restaurant;
         name = name.replace(/[-]/g, ' ');
@@ -8,11 +8,11 @@ module.exports = function(app, yelp, db) {
         const input = {
             restaurant: name.toUpperCase(),
             latitude: req.query.latitude,
-            longitude: req.query.longitude 
-          };
-        res.render('./Partials/_map',{
+            longitude: req.query.longitude
+        };
+        res.render('./Partials/_map', {
             input: input
-        });    
+        });
         //res.render('./Home/Welcome'); //relative to the root view folder
     })
 
@@ -22,32 +22,32 @@ module.exports = function(app, yelp, db) {
         var alias = req.body.alias; //post parameter
         console.log(alias);
 
-        db.restaurant.checkRestaurant(id,function(err, doesExist){
-            if(err){
+        db.restaurant.checkRestaurant(id, function (err, doesExist) {
+            if (err) {
                 console.log('caught error in checkRestaurant');
                 res.redirect('/');
                 return;
-            }           
-            if(doesExist){
+            }
+            if (doesExist) {
                 console.log('exists');
                 //res.redirect
                 res.redirect('/Restaurant?alias=' + alias);
                 return;
-            } 
+            }
             else {
                 console.log('does not exists');
 
-                var search = yelp.SearchRestaurant(id);               
-                search.then(function(response){
+                var search = yelp.SearchRestaurant(id);
+                search.then(function (response) {
                     //console.log(response.jsonBody);
                     return response.jsonBody;
-                }).then(function(result){
+                }).then(function (result) {
                     //console.log(result);
                     result.name = result.name.replace('\'', '\\\'');
                     db.restaurant.insertRestaurant(result.id, result.name, result.alias, result.location.display_address, result.location.city, result.location.state,
                         result.location.zip_code, result.coordinates.latitude, result.coordinates.longitude, result.display_phone, result.url,
-                        function(err){
-                            if(err){
+                        function (err) {
+                            if (err) {
                                 console.log('caught error in insertRestaurant');
                                 res.redirect('/');
                                 return;
@@ -56,37 +56,47 @@ module.exports = function(app, yelp, db) {
                             res.redirect('/Restaurant?alias=' + alias);
                             return;
                         });
-                }).catch(function(err){
+                }).catch(function (err) {
                     console.log('SearchRestaurant error: ');
                     res.redirect('/');
                     return;
                 });
             }
-        })   
+        })
     })
 
     app.get('/Restaurant/', function (req, res) {
         var alias = req.query.alias;
-        db.restaurant.getRestaurantInfoByAlias(alias, function(err, restaurant){
-            if(err){
+        db.restaurant.getRestaurantInfoByAlias(alias, function (err, restaurant) {
+            if (err) {
                 console.log("error when calling getRestaurantInfoByAlias");
                 res.status(404);
                 res.render('./Error/404', { url: req.url });
                 return;
             }
-            res.render('./Restaurant/Profile',{
-                profile: restaurant
-            }); 
-            return;
+            db.restaurant.getReviews(restaurant.restID, function (err, result) {
+                if (err) {
+                    console.log("error when calling getReviews");
+                    res.status(404);
+                    res.render('./Error/404', { url: req.url });
+                    return;
+                }
+                res.render('./Restaurant/Profile', {
+                    profile: restaurant,
+                    reviews: result
+
+                });
+                return;
+            })
         })
-         
+
     })
 
     app.post('/Restaurant/Rating', function (req, res) {
         console.log('calling Rating');
         var yelpID = req.body.id;
-        db.restaurant.getRestaurantID(yelpID, function(err, restID){
-            if(err){
+        db.restaurant.getRestaurantID(yelpID, function (err, restID) {
+            if (err) {
                 console.log("error when calling getRestaurantID");
                 var rating = {
                     count: 0,
@@ -96,7 +106,7 @@ module.exports = function(app, yelp, db) {
                 return;
             }
             console.log('returning rating result');
-            db.restaurant.getRatingSumAndCount(restID, function(err, count, sum){
+            db.restaurant.getRatingSumAndCount(restID, function (err, count, sum) {
                 var rating = {
                     count: count,
                     sum: sum
@@ -105,10 +115,10 @@ module.exports = function(app, yelp, db) {
                 return;
             })
 
-        }) 
+        })
     })
 
-    app.post('/Restaurant/Review', function(req, res){
+    app.post('/Restaurant/Review', function (req, res) {
         var restID = req.body.restID;
         var title = req.body.title;
         var description = req.body.description;
@@ -116,13 +126,13 @@ module.exports = function(app, yelp, db) {
 
         var userID = req.session.id;
 
-        db.restaurant.insertReview(restID, userID, title, description, rating, function(err){
-            if(err){
+        db.restaurant.insertReview(restID, userID, title, description, rating, function (err) {
+            if (err) {
                 console.log("error when calling insertReview");
-                return; 
+                return;
             }
             res.redirect('/');
         })
     })
-    
+
 }
